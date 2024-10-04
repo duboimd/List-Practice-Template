aec9866ba5db993196db77baee9c609ce5a74d60
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-10-04 10:07:19 -0600

    Create List Practice.py

---
+++ List Practice.py
@@ -0,0 +1,36 @@
+# LISTS:
+print("Number 1:")
+# Q1: Create a list of 4 types of drinks named drinks. Then, print drinks.
+
+print()
+print("Number 2:")
+# Q2: Create a list of 6 school subjects called schoolSubjects. Then, print schoolSubjects.
+
+print()
+print("Number 3:")
+# Q3: Create a list of 5 genres of movies called movieGenres. Then, print movieGenres.
+
+print()
+print("Number 4:")
+# Q4: Create a list called foods that contains 4 of your favourite foods. Then, print foods.
+
+print()
+print("Number 5:")
+# Q5: Create a list called colors that contains 5 random colors. Then, print colors.
+
+print()
+print("Number 6:")
+# Q6: Create a list called countries that contains 6 countries. Then, print countries. Lastly, print the length of countries (hint: use the function len)
+
+print()
+print("Number 7:")
+# Q7: Create a list called movies that contains 4 movies. Then, print movies. After, print the length of movies (hint: use the function len). Lastly, print the first item (hint: use indexing)
+
+print()
+print("Number 8:")
+# Q8. Create a list that contains 5 of your favourite hobbies.
+# a. Print the list. 
+# b. Print the length of the list.
+# c. Print index 2.
+# d. Print the first item (use indexing).
+
